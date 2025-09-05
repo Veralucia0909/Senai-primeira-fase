778f3938fe13d873cf00a40c3bea33ca1d653ea5
Veralucia0909 <veraluciadev@outlook.com>
2025-09-04 21:55:07 -0300

    Aula 2 javascrip

--- portugol/script.js
+++ portugol/script.js
@@ -1,2 +1,27 @@
-alert("olá mundo!")
+// alert("olá mundo!")
+// alert("Oi")
+// alert("Tudo bem?")
+// alert("Você vem sempre aqui?")
+
+// let idade
+
+// idade = 18
+// alert(idade)
+
+// idade = "dezoite"
+// alert(idade)
+
+// idade =18.1
+// alert(idade)
+
+const Curso = "Desenvolvimento de Sistema"
+alert(Curso)
+
+alert(Curso)
+let Curso2 = "Desistir"
+
+var antigao = "Nunca desistir"
+
+
+
 
