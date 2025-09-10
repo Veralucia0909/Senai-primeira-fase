55097a6b6d681fe1700ad6e4a6feda5f095de5d7
Veralucia0909 <veraluciadev@outlook.com>
2025-09-09 22:13:21 -0300

    variaveis

---
+++ logica-js/variaveis.js
@@ -0,0 +1,27 @@
+//variavel com var
+// var nome = "Vera" //string é um texto
+// // var nome = "Lima"
+// nome = "Vera"
+
+
+//variavel com let
+// let nome = " Vera"
+// nome = "Lima"
+
+
+// variavel const
+const nome = "Vera"  //string
+const sobrenome = "Lima"
+const idade = 35  //number
+const feminino = true  //boolean
+
+//variavel array
+const frutas = ["laranja", "maçã", "banana"]
+
+console.log(frutas[0])
+
+// console.log("Meu nome é " + nome + " " + sobrenome + " e minha idade é "  + idade + " anos ")
+
+// console.log(`Meu nome é ${nome} ${sobrenome} e minha idade é ${idade} anos`)   //  tepleit string
+
+
