1dd7749abf136b065bf961e008790071baef2bfe
Veralucia0909 <veraluciadev@outlook.com>
2025-09-30 19:07:03 -0300

    repeticoes

---
+++ aula-javascript/repeticoes/script.js
@@ -0,0 +1,16 @@
+
+// let acertos = Number(prompt("Quantos acertos?"))
+// let valorPorQuestao = 10/16
+// let nota = acertos * valorPorQuestao
+
+// alert("Sua nota Final: " + nota)
+
+
+let contador = 0
+while(contador < 5){
+    contador = contador + 1
+    console.log("Contador: " + contador)
+   
+
+
+}
