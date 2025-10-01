221e6d51d17cbb171faa361e9e5869b8480cba82
Veralucia0909 <veraluciadev@outlook.com>
2025-09-30 22:09:17 -0300

    funções

---
+++ logica-js/funcoes.js
@@ -0,0 +1,23 @@
+// function saudacao(nome){
+//     console.log(`Bom dia! ${nome}`)
+// }
+
+
+// const saudacao = (nome)=>{
+//     console.log(`Bom dia! ${nome}`)
+// }
+
+// saudacao("Vera")
+// saudacao("Maria")
+
+// const soma = (x,y) =>{
+//      x+y return x+y
+// } 
+
+// const soma = (x,y) => x+y return  // => arrow function
+
+// console.log(soma(2,2));
+
+const soma = (x, y) => x + y;
+
+console.log(soma(2, 2)); 
