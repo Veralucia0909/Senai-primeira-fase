6577171e8ea20c7b2095d7e0e4ce7749988922e5
Veralucia0909 <veraluciadev@outlook.com>
2025-10-16 21:49:30 -0300

    While

---
+++ aula-javascript/desafio5/ex_do_while_prof.js
@@ -0,0 +1,13 @@
+let i = 1;
+do {
+    console.log ("Número com do.. while:", i);
+    i++;
+}while (i <= 5)
+
+    
+
+    let i = 0
+    do {
+    console.log ("Número com do.. while:", i);
+    i++;
+}while (i < 100)
---
+++ aula-javascript/desafio5/switch_prof.js
@@ -0,0 +1,34 @@
+// let dia = "Sexta"
+// switch (dia) {
+//     case "Segunda":
+//         console,log("Segundou");
+//         break;
+//         case "Sexta":
+//             console.log("Sextou");
+//             break;
+//             default;
+//             console.log("Opção Invalida");
+//             break
+// }
+
+
+// faça uma variavel de dia (numerica) deixando a variavel em evidencia, dependendo do numero digitado, apresente o dia da samana, caso seja inexistente 
+// apresente uma mensagem padrão qualquer (que faça jus a situação)
+
+
+let dia = 1;
+
+switch (dia) {
+    case 1:
+        console.log("Domingo");
+        break;
+    case 2:
+        console.log("Segunda-feira");
+        break;
+    default:
+        console.log("Opção inválida");
+        break;
+}
+
+
+
