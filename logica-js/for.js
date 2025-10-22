e301c7b323128f0ef435b501061b0c11e9537103
Veralucia0909 <veraluciadev@outlook.com>
2025-10-21 22:16:21 -0300

    dom

--- logica-js/for.js
+++ logica-js/for.js
@@ -1,38 +1,42 @@
-for(let i = 0; i <=10; i++){
-    console.log(i)
-}
+// for(let i = 0; i <=10; i++){
+//     console.log(i)
+// }
 
 
-for(let i = 10; i >=0; i--){
-    console.log(i)
+// for(let i = 10; i >=0; i--){
+//     console.log(i)
     
-}
+// }
 
-for(let i = 0; i <=20; i+=2){
+// for(let i = 0; i <=20; i+=2){
+//     console.log(i)
+
+// }
+   
+// let soma = 0
+
+// for(let i = 1; i<100; i++){
+//     soma += i
+// }
+   
+// console.log("resultado da soma ", soma)
+
+
+// let alunos = ["João","Maria","Joana"]
+// alunos.push("Pedro")
+
+// for(let i = 0; i <alunos.length; i++){
+//     console.log("alunio: ", alunos[i])
+// }
+
+// console.log(alunos.length)
+
+// let frutas = ["Morango","Maça","Pera","Kiwi","Banana"]
+
+// for(let i = 0; i <frutas.length; i++){
+//     console.log("fruta: ", frutas[i])
+// }
+
+for(let i = 0; i >=100; i++){
     console.log(i)
-
-}
-   
-let soma = 0
-
-for(let i = 1; i<100; i++){
-    soma += i
-}
-   
-console.log("resultado da soma ", soma)
-
-
-let alunos = ["João","Maria","Joana"]
-alunos.push("Pedro")
-
-for(let i = 0; i <alunos.length; i++){
-    console.log("alunio: ", alunos[i])
-}
-
-console.log(alunos.length)
-
-let frutas = ["Morango","Maça","Pera","Kiwi","Banana"]
-
-for(let i = 0; i <frutas.length; i++){
-    console.log("fruta: ", frutas[i])
 }
